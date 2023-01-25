bcc5c64165eb3c444292619b7d43df86e111cdfa
LEEGWONSEOK <leestart1023@gmail.com>
2023-01-26 00:47:54 +0900

    Feat: Users, Recipes 기본 S, C, M 생성 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, RecipesModule],
 })
 export class AppModule {}

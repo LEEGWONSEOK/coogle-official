d546502f43f392cfb0373f58d8e5439d46f002d7
LEEGWONSEOK <leestart1023@gmail.com>
2023-01-26 18:01:35 +0900

    Feat: 서버 포트 확인 콘솔로그 생성 완료

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3003);
+  console.log(`Serving on port 3003`);
 }
 bootstrap();
